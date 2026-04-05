c2d7ee0d138af61d323afc5c957d398cedbf88ef
PD45-46 <deevipranav5@gmail.com>
2026-04-05 10:07:07 +1000

    Added vga text

--- custom_kernel/src/main.rs
+++ custom_kernel/src/main.rs
@@ -5,24 +5,23 @@
 #![no_main] // disable all rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 // -> ! is a divergent function that never returns to the caller
 pub extern "C" fn _start() -> ! { // using C calling conventions
-    let vga_buffer = 0xb8000 as *mut u8;
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World! {}", "Testing");
 
     loop {}
 }
+
+
+
