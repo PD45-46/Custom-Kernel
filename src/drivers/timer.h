a196be7c3cd51bcbbc84ab0b0daa3f6cc03b71d3
PD45-46 <deevipranav5@gmail.com>
2026-04-14 23:01:39 +1000

    Done timer

---
+++ src/drivers/timer.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h> 
+
+/*
+Every tick of the timer is a 'chance' to switch from one process to another. 
+*/
+
+void timer_init(uint32_t frequency);
+uint64_t timer_ticks();  
