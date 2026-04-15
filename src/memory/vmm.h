9c198f8187f5f02fe58e564785a2210e849b0cea
PD45-46 <deevipranav5@gmail.com>
2026-04-15 20:08:01 +1000

    Done Virtual Mem Manager

--- src/memory/vmm.h
+++ src/memory/vmm.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <stdint.h> 
 
+/*
+The Virtual Memory Manager (VMM) manages the CPU's page tables --- the 
+data structures that map virtual addresses to physical addresses. When 
+memory is accessed, the CPU's MMU walks the page table to translate 
+virtual space to a physical one. The VMM will let me create, modify, 
+and destroy the mappings.  
+*/
 
 #define KERNEL_VIRT_BASE 0xFFFFFFFF80000000
 
